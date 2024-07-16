28fdf3d0ff555bb953eeb1273547f55ffd8e1a38
Chaoen <calvin19961211@gmail.com>
2024-07-16 10:59:34 +0800

    add table tennis predefined datasets

--- detectron2/data/datasets/builtin.py
+++ detectron2/data/datasets/builtin.py
@@ -29,6 +29,27 @@ from .coco_panoptic import register_coco_panoptic, register_coco_panoptic_separa
 from .lvis import get_lvis_instances_meta, register_lvis_instances
 from .pascal_voc import register_pascal_voc
 
+# ==== Predefined datasets and splits for table tennis ==========
+
+_PREDEFINED_SPLITS_TABLE_TENNIS = {}
+_PREDEFINED_SPLITS_TABLE_TENNIS["table_tennis"] = {
+    "table_tennis_train": ("table_tennis/train", "table_tennis/annotations/panoptic_train.json"),
+    "table_tennis_val": ("table_tennis/val", "table_tennis/annotations/panoptic_val.json"),
+}
+
+
+def register_all_table_tennis(root):
+    for dataset_name, splits_per_dataset in _PREDEFINED_SPLITS_TABLE_TENNIS.items():
+        for key, (image_root, json_file) in splits_per_dataset.items():
+            # Assume pre-defined datasets live in `./datasets`.
+            register_coco_instances(
+                key,
+                _get_builtin_metadata(dataset_name),
+                os.path.join(root, json_file) if "://" not in json_file else json_file,
+                os.path.join(root, image_root),
+            )
+
+
 # ==== Predefined datasets and splits for COCO ==========
 
 _PREDEFINED_SPLITS_COCO = {}
@@ -190,18 +211,14 @@ def register_all_cityscapes(root):
         inst_key = key.format(task="instance_seg")
         DatasetCatalog.register(
             inst_key,
-            lambda x=image_dir, y=gt_dir: load_cityscapes_instances(
-                x, y, from_json=True, to_polygons=True
-            ),
+            lambda x=image_dir, y=gt_dir: load_cityscapes_instances(x, y, from_json=True, to_polygons=True),
         )
         MetadataCatalog.get(inst_key).set(
             image_dir=image_dir, gt_dir=gt_dir, evaluator_type="cityscapes_instance", **meta
         )
 
         sem_key = key.format(task="sem_seg")
-        DatasetCatalog.register(
-            sem_key, lambda x=image_dir, y=gt_dir: load_cityscapes_semantic(x, y)
-        )
+        DatasetCatalog.register(sem_key, lambda x=image_dir, y=gt_dir: load_cityscapes_semantic(x, y))
         MetadataCatalog.get(sem_key).set(
             image_dir=image_dir,
             gt_dir=gt_dir,
@@ -234,9 +251,7 @@ def register_all_ade20k(root):
         image_dir = os.path.join(root, "images", dirname)
         gt_dir = os.path.join(root, "annotations_detectron2", dirname)
         name = f"ade20k_sem_seg_{name}"
-        DatasetCatalog.register(
-            name, lambda x=image_dir, y=gt_dir: load_sem_seg(y, x, gt_ext="png", image_ext="jpg")
-        )
+        DatasetCatalog.register(name, lambda x=image_dir, y=gt_dir: load_sem_seg(y, x, gt_ext="png", image_ext="jpg"))
         MetadataCatalog.get(name).set(
             stuff_classes=ADE20K_SEM_SEG_CATEGORIES[:],
             image_root=image_dir,
@@ -252,6 +267,7 @@ if __name__.endswith(".builtin"):
     # Assume pre-defined datasets live in `./datasets`.
     _root = os.path.expanduser(os.getenv("DETECTRON2_DATASETS", "datasets"))
     register_all_coco(_root)
+    register_all_table_tennis(_root)
     register_all_lvis(_root)
     register_all_cityscapes(_root)
     register_all_cityscapes_panoptic(_root)
